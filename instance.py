0de686cb5ececbd2852dabf26705069ca3d7f205
AZV <arthur.dobromir@gmail.com>
2023-03-08 03:38:53 +0100

    create dedicated instance holder

---
+++ instance.py
@@ -0,0 +1,4 @@
+from Core.editor import Editor
+Editor_Instance = Editor()
+from UI.uimain import UIManager
+UIManager_Instance = UIManager()
--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-from Core.editor import Editor
-Editor_Instance = Editor()
-from UI.uimain import UIManager
-UIManager_Instance = UIManager()
-
 def main():
     UIManager_Instance.run()
 
 if __name__ == "__main__":
+    from instance import *
     main()
